290757bcc5837a792bf62930a81df6af32b74804
Ryan Eshleman <ryaneshleman@gmail.com>
2015-04-11 19:15:21 -0700

    updated setup.py, properties.py

--- setup.py
+++ setup.py
@@ -30,11 +30,13 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'atmospheres-web-service = atmospheres.server.web_service:main'
+            'atmospheres-web-service = atmospheres.server.web_service:main',
+            'atmospheres-ingest = atmospheres.ingestion.ingestionengine:main'
         ]
     },
     package_data={
-    'static': 'atmospheres/static/*'
+    'static': 'atmospheres/static/*',
+    'resources': 'atmospheres/resources/*'
     }
     
 

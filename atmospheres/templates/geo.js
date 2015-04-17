c539f7d82df5a5ac3161ce5c5d9542b6ea8611f4
Vishal Ravi Shankar <vshankar@mail.sfsu.edu>
2015-04-17 13:30:15 -0700

    new templates again for angular

--- atmospheres/templates/geo.js
+++ atmospheres/templates/geo.js
@@ -1,83 +1,104 @@
 var app = angular.module('app', [])
 
 .directive('mapViz', function() {
-    var width = 600;
-    var height = 400;
 
     return {
         restrict: 'E',
         link: function (scope, element, attrs) {
-            var viz = d3.select('body')
-                .append('div')
-                .attr('id', 'map')
+var width = 600,
+    height = 400,
+    div = d3.select('body')
+        .append('div')
+        .attr('id','map')
+        .style('width', width + 'px')
+        .style('height', height + 'px');
+            
+// Create the Google Map…
+var map = new google.maps.Map(div.node(), {
+  zoom: 12,
+  center: new google.maps.LatLng(37.76487, -122.41948),
+  mapTypeId: google.maps.MapTypeId.TERRAIN,
+  minZoom: 2  // stuff goes wrong if we allow world wraparound
+});
+
+
+          
+// Load the  data. When the data comes back, create an overlay.
+queue()
+    .defer(d3.json, "{{ url_for('static', filename='sf_zips.topo.json') }}")
+    .await(ready)
+
+var svg, overlay;
+
+function ready(error, world) {
+    var zipcodes = topojson.feature(world, world.objects.geo).features,  
+        // land = topojson.feature(world, world.objects.land);
+
+    overlay = new google.maps.OverlayView();
+    overlay.onAdd = function() {
+        // create an SVG over top of it. 
+        svg = d3.select(overlay.getPanes().overlayLayer)
+            .append('div')
+                .attr('id','d3map')
                 .style('width', width + 'px')
-                .style('height', height + 'px');
+                .style('height', height + 'px')
+            .append('svg')
+                .attr('width', width)
+                .attr('height', height);
+            
+        svg.append('g')
+            .attr('id','zipcodes')
+            .selectAll('path')
+                .data(zipcodes)
+              .enter().append('path')
+                .attr('class','zipcode')
+                .attr("id",function(d){return "zip_"+d.id})
+        
+        overlay.draw = redraw;
+        google.maps.event.addListener(map, 'bounds_changed', redraw);
+        google.maps.event.addListener(map, 'center_changed', redraw);
+    };
+    overlay.setMap(map);
 
-            var map = new google.maps.Map(viz.node(), {
-                zoom: 12,
-                center: new google.maps.LatLng(37.76487, -122.41948),
-                mapTypeId: google.maps.MapTypeId.TERRAIN,
-                min-zoom: 2
-            });
+    //this is a hack!
+    setTimeout(updateColors, 100)
+}
 
-            queue()
-                .defer(d3.json, "static/sf_zips.topo.json")
-                .await(ready);
+function updateColors()
+{
+    d3.select("#zip_94105")
+        .style('fill', 'red');
 
-            var svg, overlay;
+    d3.select("#zip_94111")
+        .style('fill', 'aquamarine');
 
-            function ready(error, world) {
-                var countries = topojson.feature(world, world.objects.geo).features,  
-                    //land = topojson.feature(world, world.objects.land);
+    d3.select("#zip_94132")
+        .style('fill', 'purple');
 
-                overlay = new google.maps.OverlayView();
-                overlay.onAdd = function() {
-                    // create an SVG over top of it. 
-                    svg = d3.select(overlay.getPanes().overlayLayer)
-                        .append('div')
-                            .attr('id','d3map')
-                            .style('width', width + 'px')
-                            .style('height', height + 'px')
-                        .append('svg')
-                            .attr('width', width)
-                            .attr('height', height);
-                        
-                    svg.append('g')
-                        .attr('id','countries')
-                        .selectAll('path')
-                            .data(countries)
-                          .enter().append('path')
-                            .attr('class','country');
-                    
-                    overlay.draw = redraw;
-                    google.maps.event.addListener(map, 'bounds_changed', redraw);
-                    google.maps.event.addListener(map, 'center_changed', redraw);
-                };
-                overlay.setMap(map);
-            }
+}
 
-            function redraw() {
-                
-                var bounds = map.getBounds(),
-                    ne = bounds.getNorthEast(),
-                    sw = bounds.getSouthWest(),
-                    projection = d3.geo.mercator()
-                        .rotate([-bounds.getCenter().lng(),0])
-                        .translate([0,0])
-                        //.center([0,0])
-                        .scale(1),
-                    path = d3.geo.path()
-                        .projection(projection);
-                        
-                var p1 = projection([ne.lng(),ne.lat()]),
-                    p2 = projection([sw.lng(),sw.lat()]);
-                
-                svg.select('#countries').attr('transform', 
-                    'scale('+width/(p1[0]-p2[0])+','+height/(p2[1]-p1[1])+')'+
-                    'translate('+(-p2[0])+','+(-p1[1])+') ');
-                          
-                svg.selectAll('path').attr('d', path);
-            }                               
+function redraw() {
+    
+    var bounds = map.getBounds(),
+        ne = bounds.getNorthEast(),
+        sw = bounds.getSouthWest(),
+        projection = d3.geo.mercator()
+            .rotate([-bounds.getCenter().lng(),0])
+            .translate([0,0])
+            //.center([0,0])
+            .scale(1),
+        path = d3.geo.path()
+            .projection(projection);
+            
+    var p1 = projection([ne.lng(),ne.lat()]),
+        p2 = projection([sw.lng(),sw.lat()]);
+    
+    svg.select('#zipcodes').attr('transform', 
+        'scale('+width/(p1[0]-p2[0])+','+height/(p2[1]-p1[1])+')'+
+        'translate('+(-p2[0])+','+(-p1[1])+') ');
+              
+    svg.selectAll('path').attr('d', path);
+    }
         }
     };
 });

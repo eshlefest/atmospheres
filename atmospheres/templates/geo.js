12ae28120bfb480620c2d6b4c4318de957c052c1
Ryan Eshleman <ryaneshleman@gmail.com>
2015-05-12 12:06:58 -0700

    fixed visualization bug

--- atmospheres/templates/geo.js
+++ atmospheres/templates/geo.js
@@ -218,8 +218,8 @@ L.control.Button = L.Control.extend({
                 // loop through our density intervals and generate a label with a colored square for each interval
                 for (var i = 0; i < grades.length; i++) {
                     div.innerHTML +=
-                        '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-                        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+</br>');
+                        '<i style="background:' + getColor(grades[i]) + '"></i> ' +
+                        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i] + '<br>' : '+</br>');
                 }
 
                 return div;
